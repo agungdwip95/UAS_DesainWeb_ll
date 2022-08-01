26da4e358643950743309d81c323522d98fa5305
agungdwip95 <agungdwiprasetiyo7@gmail.com>
2022-08-01 23:18:23 +0700

    commit pertama

---
+++ bookstore-frontend/src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+     <v-container>
+         <h1>AGUNG DWI PRASETIYO - D112011061</h1> <br>
+         <h1>FAIZAL RAMDAN - D112011065</h1> <br>
+         <h1>IBRAHIM SAWITRO YUDA - D112011015</h1> <br>
+         <h1>IF-4B</h1> 
+     </v-container> 
+  </div>
+</template>
+
